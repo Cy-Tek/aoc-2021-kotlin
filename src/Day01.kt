5c9c051e6d0a4d801956dd31a6552f83fcf45fd1
Josh Hannaford <josh.hannaford@openly.com>
2021-12-02 18:50:40 -0500

    Initial commit

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,36 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var result = 0
+        val numbers = input.map { it.toInt() }
+        for (i in 1 until numbers.size) {
+            result += if (numbers[i] > numbers[i - 1]) 1 else 0
+        }
+
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        check(input.size > 3) // We need to have at least 4 elements to check the sliding window
+
+        var result = 0
+        val numbers = input.map { it.toInt() }
+        val sums = mutableListOf<Int>()
+
+        sums.add(numbers[0] + numbers[1] + numbers[2])
+
+        for (i in 1..numbers.size - 3) {
+            sums.add(numbers[i] + numbers[i + 1] + numbers[i + 2])
+            result += if (sums[i] > sums[i - 1]) 1 else 0
+        }
+
+        return result
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("inputs/Day01_test")
+    check(part1(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInput("inputs/Day01")
     println(part1(input))
     println(part2(input))
 }
